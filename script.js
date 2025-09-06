1e8d7c111ae426b8fbc5d7b35d9cd7493538e227
Luka Popovic <lukapopovic0111@gmail.com>
2025-09-06 15:46:03 +0200

    Add prototype methods and properties to Person constructor

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const Person = function (firstName, birthYear) {
   //instance properties
   this.firstName = firstName;
   this.birthYear = birthYear;
-  //Never to this
+  //Never do this
   /*this.calcAge = function () {
     console.log(2035 - this.birthYear);
   };*/
@@ -20,3 +20,16 @@ const nikola = new Person('Nikola', 2000);
 const zoki = new Person('Zoki', 1997);
 console.log(nikola, zoki);
 console.log(luka instanceof Person);
+
+//Prototypes
+//do this
+Person.prototype.calcAge = function () {
+  console.log(2035 - this.birthYear);
+};
+console.log(Person.prototype);
+luka.calcAge();
+console.log(luka.__proto__ === Person.prototype);
+console.log(Person.prototype.isPrototypeOf(luka));
+Person.prototype.species = 'Homo Sapiens';
+console.log(luka.hasOwnProperty('firstName'));
+console.log(luka.hasOwnProperty('species'));
